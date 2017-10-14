0f9d9db72e86d866334ad122034a5b398d41b7ce
April Rivera <aprilcr87@gmail.com>
2017-10-14 15:09:17 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -19,8 +19,18 @@ teenager(20);
 
 
 function ageChecker(age) {
-
+if(age>=13 && age<=19){
+  console.log('You are a teenager!');
+}else if(age <=12) {
+  console.log('You are a kid');
+} else if(age>19){
+  console.log('You are a grown up.');
 }
+}
+ageChecker(4);
+
+
+
 
 function ternaryTeenager(age) {
 
