b1c6e3b8678628ad07ae1c316de572903c8c29b0
April Rivera <aprilcr87@gmail.com>
2017-10-14 14:58:49 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,8 @@
 function basicTeenager(age) {
-var basicteenager = 'age';
-var age = 13,14,15,16,17,18,19;
+  if(age>=13 && age<=19);{
+    console.log('You are a teenager!');
+  }
 
-if (basicTeenager==age);{
-  console.log('You are a teenager!');
 }
   basicTeenager(14);
 }
